871a5c44602785e3ce978451bef67436c916ed84
ulrich-cyber <76060199+Ulrich-Houngbo@users.noreply.github.com>
2022-09-10 02:36:14 +0100

    if_else_while

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,6 +4,11 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
+
+/**
+ *main - Entry point
+ *Return: Always 0 (Success)
+ */
 int main(void)
 {
 int n;
@@ -18,7 +23,6 @@ if ((n% 10) == 0)
 {
 printf("0\n");
 }
-
 else if ((n% 10) > 5)
 {
 printf("greater than 5\n");
@@ -26,9 +30,5 @@ printf("greater than 5\n");
 {
 printf("less than 6 and not 0\n");    
 }
-
-
-
-
 return (0);
 }
