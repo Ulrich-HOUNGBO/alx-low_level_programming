ba6bb34d9357dce692dbd52862d1bfd0ddf4f201
ulrich-cyber <76060199+Ulrich-Houngbo@users.noreply.github.com>
2022-09-14 00:48:42 +0100

    function_nested

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - prints lowercase alphabet
+ * Return: 0
+ */
+
+void print_alphabet(void);
+{
+char ch = 'a';
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+    _putchar(ch);
+}
+_putchar('\n');
+return (0);
+}
