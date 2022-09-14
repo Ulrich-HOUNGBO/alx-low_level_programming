aacf516b4444f50c9bf0732e8b6146fce939347d
ulrich-cyber <76060199+Ulrich-Houngbo@users.noreply.github.com>
2022-09-14 01:58:38 +0100

    functions_nested

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *  _abs - compute absolute value of integer
+ * @n: number to check
+ * Return: 1 if greater than 0, 0 if 0, -1 if less than 0
+ */
+int _abs(int)
+{
+int n;
+if ((n > 0) || (n == 0))
+{
+return (n);
+}
+else
+{
+return(n * -1);
+}
+}
