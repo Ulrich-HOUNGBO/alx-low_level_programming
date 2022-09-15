84e38b9480095f1af572791b644a4b3899301339
ulrich-cyber <76060199+Ulrich-Houngbo@users.noreply.github.com>
2022-09-16 00:44:38 +0100

    more_function_nested_loops

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *print_line - prints a straight line
+ *@n: parameter
+ *Return:returns nothing
+ */
+
+void print_line(int n)
+{
+times = n;
+for (times = n; times > 0; times--)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
