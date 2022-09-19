8534ddaa523289ea962d7ea905de63cb1ce23506
ulrich-cyber <76060199+Ulrich-Houngbo@users.noreply.github.com>
2022-09-20 00:15:51 +0100

    pointers,arrays,strings

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <string.h>
+
 /**
  * _strlen - returns the length of a string
  * @s: string
@@ -8,6 +8,10 @@
 int _strlen(char *s)
 {
 int len;
-len = strlen(*s);
-return len;
+while (*s != '\0')
+{
+len++;
+s++;
+}
+return (len);
 }
