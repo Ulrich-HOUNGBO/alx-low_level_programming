df5ebc7b297fb773586dab636aff86111ac95a50
ulrich-cyber <76060199+Ulrich-Houngbo@users.noreply.github.com>
2022-09-20 01:25:16 +0100

    pointers,arrays,strings

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,17 +7,26 @@
 
 void print_rev(char *s)
 {
+
 int len = 0;
 int i;
+
+/* iterate to find length of string and point to last character */
 while (*s != '\0')
 {
-len ++;
+len++;
 ++s;
 }
+
+/* go back to character before null character */
+s--;
+
+/* print string reversed */
 for (i = len; i > 0; i--)
 {
 _putchar(*s);
 s--;
 }
+
 _putchar('\n');
 }
