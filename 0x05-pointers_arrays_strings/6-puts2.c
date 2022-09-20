54f1c1d1a521d7db44f0fb46120c019777fef2bf
ulrich-cyber <76060199+Ulrich-Houngbo@users.noreply.github.com>
2022-09-20 01:43:55 +0100

    pointers,arrays,strings

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -19,10 +19,15 @@ s++;
 return (len);
 }
 
+/**
+ * puts2 - prints every second character of a string, followed by a new line
+ * @str: string to print
+ */
+
 void puts2(char *str)
 {
-int i; 
-for (i=0; str[i] != '\0' && i < _strlen(str); i += 2)
+int i;
+for (i = 0; str[i] != '\0' && i < _strlen(str); i += 2)
 {
 _putchar(str[i]);
 }
