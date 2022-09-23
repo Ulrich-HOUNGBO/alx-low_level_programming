224dc199e31f20f69fcb75784eb51e221db8738e
ulrich-cyber <76060199+Ulrich-Houngbo@users.noreply.github.com>
2022-09-23 02:09:14 +0100

    more_pointers_arrays_strings

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverse array elements
+ * @a: array
+ * @n: number of elements in array
+ */
+
+void reverse_array(int *a, int n)
+{
+int i;
+
+i = 0;
+while (i < n)
+{
+if (i != 0)
+{
+printf(", ");
+}
+printf("%d", a[i]);
+i++;
+}
+printf("\n");
+}
