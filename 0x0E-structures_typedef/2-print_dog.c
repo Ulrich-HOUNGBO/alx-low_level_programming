81554b92c87ff5815e14930a5a2f9d035a3f6e9f
ulrich-cyber <76060199+Ulrich-Houngbo@users.noreply.github.com>
2022-10-11 01:02:15 +0100

    function prints a struct dog and it's information

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,7 +13,7 @@ printf("Name: %s\n", d->name);
 }
 else
 {
-printf("Name: (nil)\n")
+printf("Name: (nil)\n");
 }
 if (d->age = NULL)
 {
@@ -21,7 +21,7 @@ printf("Age: %s\n", d->age);
 }
 else
 {
-printf("Age: (nil)\n")
+printf("Age: (nil)\n");
 }
 if (d->owner = NULL)
 {
@@ -29,6 +29,6 @@ printf("Owner: %s\n", d->owner);
 }
 else
 {
-printf("Owner: (nil)\n")
+printf("Owner: (nil)\n");
 }
 }
